e619a677c39a9881038fd91e8c02e8f800a0c7f0
sc30gsw <haidoushi875@gmail.com>
2022-06-18 06:54:39 +0900

    観光地情報を辞書に格納する処理

--- ranking.py
+++ ranking.py
@@ -24,3 +24,24 @@ spot_name = spot_name.text.replace('\n', '')
 
 # 評点
 eval_num = float(spot.find('div', attrs={'class': 'u_rankBox'}).text.replace('\n', ''))
+
+# 観光地情報を格納する辞書
+datum = {}
+
+# 各カテゴリー評価
+category_items = spot.find('div', attrs={'class': 'u_categoryTipsItem'})
+category_items = category_items.find_all('dl')
+
+for category_item in category_items:
+
+  print(category_item)
+  # 各カテゴリーの要素を取得
+  category = category_item.dt.text
+  rank = float(category_item.span.text)
+  
+  # カテゴリー情報を辞書に格納
+  datum[category] = rank
+
+# 観光地情報を辞書に格納
+datum['観光地名'] = spot_name
+datum['評点'] = eval_num
