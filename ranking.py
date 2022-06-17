a4c5eb38c805c93386f816667035b8bbdd8e1ade
sc30gsw <haidoushi875@gmail.com>
2022-06-18 06:18:33 +0900

    レスポンス情報を取得

--- ranking.py
+++ ranking.py
@@ -2,3 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 import pandas as pd
+
+
+url = 'https://scraping-for-beginner.herokuapp.com/ranking/'
+
+# レスポンス取得
+res = requests.get(url)
+
+# BeautifulSoupでレスポンスをHTMLの構造に変換する
+soup = BeautifulSoup(res.text, 'html.parser')
