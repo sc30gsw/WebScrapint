c8e0dbb459bfe349b13df5cdc15057d4c7ee62e7
sc30gsw <haidoushi875@gmail.com>
2022-06-18 06:29:55 +0900

    観光地名の取得

--- ranking.py
+++ ranking.py
@@ -11,3 +11,13 @@ res = requests.get(url)
 
 # BeautifulSoupでレスポンスをHTMLの構造に変換する
 soup = BeautifulSoup(res.text, 'html.parser')
+
+# 1つの観光地情報の取得
+spots = soup.select('.u_areaListRankingBox')
+spot = spots[0]
+
+# 観光地名
+spot_name = spot.find('div', attrs={'class': 'u_title'})
+# 観光地のタイトルからspanを抽出する
+spot_name.find('span', attrs={'class': 'badge'}).extract()
+spot_name = spot_name.text.replace('\n', '')
